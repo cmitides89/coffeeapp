43f941616427eb8a1b044358f9bfa0b66b2bd58b
Constantin <mitides.constantin@gmail.com>
2013-04-22 09:11:48 -0400

    added rating to html page

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,6 @@ class Location < ActiveRecord::Base
 has_many :caves
   attr_accessible :address, :latitude, :longitude
   geocoded_by :address
+  validate :address, :presence => true
   after_validation :geocode, :if => :address_changed?
 end
